b0067167ea5b7a5ccb496fb52158ee484ae88d2b
Vladislav Romanovsky <VRomanovsky@slb.com>
2021-04-22 14:57:49 +0300

    Access tokens for deps

--- startup.user.js
+++ startup.user.js
@@ -6,13 +6,11 @@
 // @author       You
 // @match        https://staffing.epam.com/positions/153695794/proposals
 // @icon         https://www.google.com/s2/favicons?domain=epam.com
-
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
-
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/startup.js
-// @resource     IMPORTED_CSS http://cdn.abc.com/startup.css
+// @require      https://x-token-auth:CR5C5eauPYEfYu5CHf@bitbucket.org/FrostBy/startup/raw/6f5b789cf0aff23f3509fd763ec52e2a052fe104/startup.js
+// @resource     IMPORTED_CSS https://x-token-auth:CR5C5eauPYEfYu5CHf@bitbucket.org/FrostBy/startup/raw/6f5b789cf0aff23f3509fd763ec52e2a052fe104/startup.css
 // ==/UserScript==
 
 (function() { GM_addStyle(GM_getResourceText('IMPORTED_CSS'));})();
