0d0966f6707decfd785976ad35d9624796d10146
Vladislav Romanovsky <vladislav_romanovsky@epam.com>
2021-05-03 18:42:23 +0300

    Redesign Page reload after 30 mins of inactivity

--- startup.js
+++ startup.js
@@ -1,86 +1,104 @@
-(function() {
-  'use strict';
-  const myVar = setInterval(setColor, 1000);
+(function (events, handler) {
+    'use strict';
 
-  function setColor() {
-    console.log('3sec');
-    const isMobileVersion = document.getElementsByClassName('grid-cell-rect');
-    if (isMobileVersion.length > 0) {
-      stopColor();
+    let positionsArray = [];
+
+    const positionsInterval = setInterval(() => {
+        const positions = $('.profile-table tbody.ng-star-inserted');
+
+        if (!positions.length) return;
+        if ($('.grid-cell-rect').length > 0) initFilters();
+
+        clearInterval(positionsInterval);
+    }, 1000);
+
+    let idleTime = 0;
+    //Increment the idle time counter every minute.
+    const idleInterval = setInterval(() => {
+        idleTime++;
+        if (idleTime >= 30) window.location.reload(); //30 minutes
+    }, 60000); // 1 minute
+
+    //Zero the idle timer on mouse movement.
+    $(document).on('mousemove keypress', () => { idleTime = 0; });
+
+    function initFilters() {
+        const filters = $('<div class="filters-container open">');
+        filters
+            .append(`<div class='filter new' data-status="new">0</div>`)
+            .append(`<div class='filter offer-preparation' data-status="offer-preparation">0</div>`)
+            .append(`<div class='filter offer-acceptance' data-status="offer-acceptance">0</div>`)
+            .append(`<div class='filter on-hold' data-status="on-hold">0</div>`)
+            .append(`<div class='filter done' data-status="done">0</div>`)
+            .append(`<div class='reset'>Reset</div>`)
+            .append(`<div class='toggler open'></div>`);
+
+        $('body').append(filters);
+
+        function setClasses() {
+            $('.profile-table tbody:has(span[title="Backup Consideration"])')
+                .addClass('proposal backup-consideration new')
+                .data('status', 'new');
+            $('.profile-table tbody:has(span[title="Select Action"])')
+                .addClass('proposal select-action new')
+                .data('status', 'new');
+            $('.profile-table tbody:has(span[title="On Hold"])')
+                .addClass('proposal on-hold')
+                .data('status', 'on-hold');
+            $('.profile-table tbody:has(span[title="Offer Preparation"])')
+                .addClass('proposal offer-preparation')
+                .data('status', 'offer-preparation');
+            $('.profile-table tbody:has(span[title="Offer Acceptance"])')
+                .addClass('proposal offer-acceptance')
+                .data('status', 'offer-acceptance');
+            $('.profile-table tbody:has(span[title="Background Check"])')
+                .addClass('proposal background-check done')
+                .data('status', 'done');
+        }
+
+        function calculateStages() {
+            $('.filter.done')
+                .html($('tbody.background-check').length)
+                .prop('title', 'Background Check');
+            $('.filter.on-hold')
+                .html($('tbody.on-hold').length)
+                .prop('title', 'On Hold');
+            $('.filter.offer-acceptance')
+                .html($('tbody.offer-acceptance').length)
+                .prop('title', 'Offer Acceptance');
+            $('.filter.offer-preparation')
+                .html($('tbody.offer-preparation').length)
+                .prop('title', 'Offer Preparation');
+            $('.filter.new')
+                .html($('tbody.new').length)
+                .prop('title', 'New')
+        }
+
+        function resetFilters() {
+            $('.filter.done, .filter.offer-preparation').not('.disabled').trigger('click');
+            $('.filter.new.disabled, .filter.offer-acceptance.disabled, .filter.on-hold.disabled').trigger('click');
+        }
+
+
+        $('.reset').on('click', () => {
+            resetFilters();
+        })
+
+        $('.toggler').on('click', () => {
+            $('.filters-container').toggleClass('open');
+        })
+
+        $('.filter').on('click', function () {
+            $(this).toggleClass('disabled');
+
+            const status = $(this).data('status');
+            $('.profile-table tbody').each(function () {
+                if ($(this).data('status') === status) $(this).toggle();
+            })
+        });
+
+        setClasses();
+        calculateStages();
+        resetFilters();
     }
-  }
-
-  $('body').
-      append(`'<p class='cir nuwo'></p>'`).
-      append(`'<p class='cir opr'></p>'`).
-      append(`'<p class='cir oac'></p>'`).
-      append(`'<p class='cir ohl'></p>'`).
-      append(`'<p class='cir done'></p>'`).
-      append(`'<p class='rest1'></p>'`);
-
-  $('.cir.done').addClass('hihi');
-  $('.cir.opr').addClass('hihi');
-
-  $('.cir').on('click', function(event) {
-    $(this).toggleClass('hihi');
-    if ($(event.target).hasClass('done')) $(
-        '.profile-table tbody:has(span[title="Background Check"])').toggle();
-    if ($(event.target).hasClass('opr')) $(
-        '.profile-table tbody:has(span[title="Offer Preparation"])').toggle();
-    if ($(event.target).hasClass('oac')) $(
-        '.profile-table tbody:has(span[title="Offer Acceptance"])').toggle();
-    if ($(event.target).hasClass('ohl')) $(
-        '.profile-table tbody:has(span[title="On Hold"])').toggle();
-
-  });
-
-  function calculate() {
-    $('p.done').
-        html($('span[title="Background Check"]').length).
-        prop('title', 'Background Check');
-    $('p.ohl').
-        html($('span[title="On Hold"]').length).
-        prop('title', 'On Hold"');
-    $('p.oac').
-        html($('span[title="Offer Acceptance"]').length).
-        prop('title', 'Offer Acceptance');
-    $('p.opr').
-        html($('span[title="Offer Preparation"]').length).
-        prop('title', 'Offer Preparation');
-    $('p.nuwo').
-        html($('span[title="Backup Consideration"]').length +
-            $('span[title="Select Action"]').length).
-        prop('title', 'New');
-  }
-
-  function countr() {
-    $('.profile-table tbody:has(span[title="Backup Consideration"])').
-        addClass('nw');
-    $('.profile-table tbody:has(span[title="Select Action"])').addClass('nw');
-    $('.profile-table tbody:has(span[title="On Hold"])').addClass('oh');
-    $('.profile-table tbody:has(span[title="Offer Preparation"])').
-        addClass('op');
-    $('.profile-table tbody:has(span[title="Offer Acceptance"])').
-        addClass('oa');
-    $('.profile-table tbody:has(span[title="Background Check"])').
-        addClass('bch');
-  }
-
-  function stopColor() {
-    clearInterval(myVar);
-    $('.profile-table tbody:has(span[title="Background Check"])').hide();
-    $('.profile-table tbody:has(span[title="Offer Preparation"])').hide();
-    countr();
-    calculate();
-  }
-
-  document.addEventListener('scroll', function(event) {
-    countr();
-    calculate();
-  }, true);
-
-  $('.rest1').on('click', function() {
-    stopColor();
-  });
-
 })();
--- startup.user.js
+++ startup.user.js
@@ -8,9 +8,15 @@
 // @icon         https://www.google.com/s2/favicons?domain=epam.com
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
+// @grant        GM_setValue
+// @grant        GM_getValue
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js
 // @require      https://x-token-auth:CR5C5eauPYEfYu5CHf@bitbucket.org/FrostBy/startup/raw/6f5b789cf0aff23f3509fd763ec52e2a052fe104/startup.js
 // @resource     IMPORTED_CSS https://x-token-auth:CR5C5eauPYEfYu5CHf@bitbucket.org/FrostBy/startup/raw/6f5b789cf0aff23f3509fd763ec52e2a052fe104/startup.css
 // ==/UserScript==
 
-(function() { GM_addStyle(GM_getResourceText('IMPORTED_CSS'));})();
+(function() {
+    window.GM_setValue = GM_setValue;
+    window.GM_getValue = GM_getValue;
+    GM_addStyle(GM_getResourceText('IMPORTED_CSS'));
+})();
