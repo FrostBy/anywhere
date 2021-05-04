303d10a59750b1808a0a08b167a1d84444dc613a
Vladislav Romanovsky <vladislav_romanovsky@epam.com>
2021-05-04 18:14:13 +0300

    Version change Fixed refresh button appearance

--- startup.user.js
+++ startup.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Startup
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.41
 // @description  The script helps to keep track of updates
 // @author       Dmitry Scherbatykh
 // @contributor  Vladislav Romanovsky
