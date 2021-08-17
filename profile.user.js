43db7c294700eb3f1cf3c4653f83d4f18b61538a
Vladislav Romanovsky <frost.by@gmail.com>
2021-08-17 12:00:22 +0300

    DOM shared service loading priority

--- profile.user.js
+++ profile.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Applicant/User Profile
 // @namespace    http://tampermonkey.net/
-// @version      2021.08.17
+// @version      2021.08.17.01
 // @description  The script helps to work with an applicant/employee profile
 // @author       Vladislav Romanovsky
 // @match        https://staffing.epam.com/applicant*/*
@@ -15,8 +15,8 @@
 // @updateURL    https://github.com/FrostBy/anywhere/raw/master/profile.user.js
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/tooltipster/4.2.8/js/tooltipster.bundle.min.js
-// @require      https://github.com/FrostBy/anywhere/raw/master/services/dom/dom.profile.js
 // @require      https://github.com/FrostBy/anywhere/raw/master/services/dom/dom.shared.js
+// @require      https://github.com/FrostBy/anywhere/raw/master/services/dom/dom.profile.js
 // @require      https://github.com/FrostBy/anywhere/raw/master/services/salary.js
 // @require      https://github.com/FrostBy/anywhere/raw/master/services/version.js
 // @require      https://github.com/FrostBy/anywhere/raw/master/services/config.js
